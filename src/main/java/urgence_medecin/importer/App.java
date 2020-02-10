723137cf5e618c25c7814dcb1a8e4f9745036eeb
khoa <khoa@DESKTOP-531D76G>
2020-02-10 23:48:59 +0100

    Create test with test file

---
+++ src/main/java/urgence_medecin/importer/App.java
@@ -0,0 +1,10 @@
+package urgence_medecin.importer;
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+}
---
+++ src/test/java/urgence_medecin/importer/AppTest.java
@@ -0,0 +1,50 @@
+package urgence_medecin.importer;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.poi.EncryptedDocumentException;
+import org.apache.poi.openxml4j.exceptions.InvalidFormatException;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.WorkbookFactory;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase {
+	private static final String SAMPLE_XLSX_FILE_PATH = "BDD.xlsx";
+
+	/**
+	 * Create the test case
+	 *
+	 * @param testName name of the test case
+	 */
+	public AppTest(String testName) {
+		super(testName);
+	}
+
+	/**
+	 * @return the suite of tests being tested
+	 */
+	public static Test suite() {
+		return new TestSuite(AppTest.class);
+	}
+
+	/**
+	 * Rigourous Test :-)
+	 * 
+	 * @throws IOException
+	 * @throws InvalidFormatException // * @throws EncryptedDocumentException
+	 */
+	public void testImportExcelFIle() throws EncryptedDocumentException, InvalidFormatException, IOException {
+		// Creating a Workbook from an Excel file (.xls or .xlsx)
+		System.out.println(new File(getClass().getClassLoader().getResource(SAMPLE_XLSX_FILE_PATH).getFile()));
+		Workbook workbook = WorkbookFactory
+				.create(new File(getClass().getClassLoader().getResource(SAMPLE_XLSX_FILE_PATH).getFile()));
+		assertTrue(true);
+	}
+}
